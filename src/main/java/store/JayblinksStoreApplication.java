b0f69d9f63c54492f85dad94c56285222bb0bb86
okoro Jeremiahm <okorojeremiah91@gmail.com>
2022-12-07 06:13:29 +0100

    made some changes to the class name

--- src/main/java/store/JayblinksStoreApplication.java
+++ src/main/java/store/JayblinksStoreApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class UnicornStoreApplication {
+public class JayblinksStoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UnicornStoreApplication.class, args);
+        SpringApplication.run(JayblinksStoreApplication.class, args);
     }
 
 }
